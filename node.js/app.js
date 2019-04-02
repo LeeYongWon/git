695189c919c0e1d28f25807b3c63592c376cd9c9
dyddnjs0982 <dyddnjs0982@naver.com>
2019-04-02 14:24:10 +0900

    querystring을 express에서 다루는 법 queryString은 어떤 애플리캐이션에게 정보를 전달할때 쓴다.

--- node.js/app.js
+++ node.js/app.js
@@ -1,7 +1,37 @@
 var express = require('express');
 var app = express();
+app.locals.pretty = true;
+app.set('view engine', 'jade');
+//이건 생략 가능 그냥 views 폴더를 찾아줌
+app.set('views', './views');
 app.use(express.static('public'));
 
+
+
+app.get('/topic', function(req, res) {
+   var topics = [
+      'javascript is ...',
+      'Nodejs is...',
+      'Express is...'
+   ];
+   var output = `
+      <a href ='topic?id=0'>Javascript</a><br>
+      <a href ='topic?id=1'>Nodejs</a><br>
+      <a href ='topic?id=2'>Express</a><br>
+      ${topics[req.query.id]}
+   `
+   res.send(output);
+
+});
+app.get('/templet', function(req, res) {
+   /*첫번째 인자로 템플릿의 이름전달 데이터를
+    전달하고 싶으면 객체로 전달
+   */
+   res.render('temp', {
+      time: Date(),
+      _title: 'Jade'
+   });
+});
 
 app.get('/', function(req, res) {
    res.send('Hello home page');
@@ -31,7 +61,7 @@ app.get('/dynamic', function(req, res) {
 
    res.send(output);
 
-})
+});
 app.get('/route', function(req, res) {
    res.send('Hello Router, <img src="/route.png">');
 });
