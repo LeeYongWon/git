67a2c8d39f159759c8272e136a70789eda0f7e02
dyddnjs0982 <dyddnjs0982@naver.com>
2019-05-03 17:05:32 +0900

    객체의 값을 바꾸는 방법과 삭제 방법

--- OOP_javaScript/object.js
+++ OOP_javaScript/object.js
@@ -7,6 +7,14 @@ var memberObject={
    developer:'grahittie',
    designer:'leezhce'
 };
+//오브젝트의 값을 바꾸는 방법 두가지
+memberObject.designer='leezhe';
+console.log(memberObject.designer);
+memberObject['developer']='이용원';
+console.log(memberObject['developer']);
 //오브젝트의 값을 가져오는데 두가지 방법이 있다.
 console.log('첫번째 방법 : '+memberObject.manager);
 console.log('두번째 방법 : '+memberObject['manager']);
+//오브젝트의 값을 삭제하는 방법
+delete memberObject.manager;
+console.log(memberObject.manager);
