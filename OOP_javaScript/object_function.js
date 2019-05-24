0605d4c47ce7c0564eb7685f32aff6689ad21377
dyddnjs0982 <dyddnjs0982@naver.com>
2019-05-24 18:36:41 +0900

    prototype과 __proto__

--- OOP_javaScript/object_function.js
+++ OOP_javaScript/object_function.js
@@ -7,3 +7,6 @@ function sum(prefix) {
 //sum();가 똑같은것이다 모든 메소드는 call이라는 함수 사용가능하다.
 console.log("sum.call(kim)   ", sum.call(kim, '30 '));
 console.log("sum.call(lee)   ", sum.call(lee,'===> '));
+//this를 쓸 새로운 함수를 만든다고 보면 된다.
+var kimSum=sum.bind(kim, '-> ');
+console.log('kimSum()', kimSum());
